0b62fc74572eea9c052d442a3f8292563f4849a1
paulac00005 <lithiumer13@gmail.com>
2016-02-17 08:34:54 +0800

    asdf

--- app/src/main/java/com/example/paulac/cis/Login.java
+++ app/src/main/java/com/example/paulac/cis/Login.java
@@ -6,9 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
@@ -16,7 +23,7 @@ public class Login extends AppCompatActivity {
     private Button login;
     private RequestQueue requestQueue;
     private static final String url = "http://10.4.101.44/sbs/login.php";
-    private StringRequest stringRequest;
+    private StringRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,28 @@ public class Login extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        HashMap<String,String> hashMap = new HashMap<String, String>();
+                        hashMap.put("username",username.getText().toString());
+                        hashMap.put("password",password.getText().toString());
+
+                        return hashMap;
+                    }
+                };
+
+                requestQueue.add(request);
 
             }
         });
