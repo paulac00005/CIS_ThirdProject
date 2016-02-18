53fd38362f70c799ee421404eef2baa2c6291ecb
paulac00005 <lithiumer13@gmail.com>
2016-02-18 13:49:35 +0800

    asdf

--- app/src/main/java/com/example/paulac/cis/Login.java
+++ app/src/main/java/com/example/paulac/cis/Login.java
@@ -1,16 +1,36 @@
 package com.example.paulac.cis;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
-    private EditText username, password;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText Username, Password;
     private Button login;
-    private static final String url = "http://10.4.101.44/sbs/login.php";
+    private static final String login_url = "http://10.4.101.44/sbs/login.php";
     protected final String key = "70930f27";
+
+    InputStream is1;
 
 
     @Override
@@ -18,9 +38,71 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
+        Username = (EditText) findViewById(R.id.username);
+        Password = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.login) {
+            SignUpTask signUpTask = new SignUpTask();
+            signUpTask.execute(login_url);
+        }
+    }
+
+
+    private class SignUpTask extends AsyncTask<String, Void, Boolean> {
+
+        ProgressDialog dialog = new ProgressDialog(Login.this);
+
+        @Override
+        protected void onPreExecute() {
+            dialog.setMessage("Sending Data . . .");
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if(result == true){
+                Toast.makeText(Login.this, "Inserted", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(Login.this, "Error inserting data . . .", Toast.LENGTH_SHORT).show();
+            }
+            dialog.dismiss();
+        }
+
+        Boolean result = false;
+
+        String u = Username.getText().toString();
+        String p = Password.getText().toString();
+
+        @Override
+        protected Boolean doInBackground(String... url) {
+
+            for (String url1 : url) {
+                try {
+                    ArrayList<NameValuePair> pairs = new ArrayList<NameValuePair>();
+                    pairs.add(new BasicNameValuePair("username", u));
+                    pairs.add(new BasicNameValuePair("password", p));
+                    pairs.add(new BasicNameValuePair("key", "70930f27"));
+
+                    HttpClient client = new DefaultHttpClient();
+                    HttpPost post = new HttpPost(url1);
+                    post.setEntity(new UrlEncodedFormEntity(pairs));
+                    HttpResponse response = client.execute(post);
+
+                    result = true;
+                } catch (UnsupportedEncodingException e) {
+                    Toast.makeText(Login.this, e.toString(), Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    Toast.makeText(Login.this, e.toString(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+            return null;
+        }
 
     }
 }
