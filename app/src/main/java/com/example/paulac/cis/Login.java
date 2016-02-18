3899a0fc8cdd71921cde6a59fe72d6f694f91bbd
paulac00005 <lithiumer13@gmail.com>
2016-02-18 08:02:36 +0800

    asdf

--- app/src/main/java/com/example/paulac/cis/Login.java
+++ app/src/main/java/com/example/paulac/cis/Login.java
@@ -2,28 +2,16 @@ package com.example.paulac.cis;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
     private EditText username, password;
     private Button login;
-    private RequestQueue requestQueue;
     private static final String url = "http://10.4.101.44/sbs/login.php";
-    private StringRequest request;
+    protected final String key = "70930f27";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,36 +22,5 @@ public class Login extends AppCompatActivity {
         password = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
 
-        requestQueue = Volley.newRequestQueue(this);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }){
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        HashMap<String,String> hashMap = new HashMap<String, String>();
-                        hashMap.put("username",username.getText().toString());
-                        hashMap.put("password",password.getText().toString());
-
-                        return hashMap;
-                    }
-                };
-
-                requestQueue.add(request);
-
-            }
-        });
     }
-
 }
